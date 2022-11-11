cd5d935995e8db3c7d5a2d636899fbf998d99271
mdonatelli22 <DonatelliME20@gcc.edu>
2022-11-11 14:19:45 -0500

    Initial Commit

---
+++ Project2-Final/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project2-Final
+//
+//  Created by Maura Donatelli on 11/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project2-Final/Project2_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project2_FinalApp.swift
+//  Project2-Final
+//
+//  Created by Maura Donatelli on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Project2_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
