e1bd660695e28213837b3e4eeed6c938249965ec
mdonatelli22 <DonatelliME20@gcc.edu>
2022-11-27 16:11:23 -0500

    Added working barcode scanner

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
+        //open sans sans serif
+        
         NavigationView{
             ZStack{
 //                Image("background")
